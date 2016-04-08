dd71ca90759348128542fa2b14ed01c06fcfba13
drumguy16 <ripley.brett@gmail.com>
2016-04-08 12:28:41 -0400

    add nature

--- bam/db/seeds.rb
+++ bam/db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 categories = Category.create([{name: 'nature'}, {name: 'inspirational'}, {name: 'sports'}, {name: 'abstract'}, {name: 'hipster'}, {name: 'celebrity'}, {name: 'trump'}])
 
-quotes_nature =  Quote.create([{content: "Blah blah blah. Emotions and stuff", category_id: Category.find_by(name: nature).id}])
+quotes_nature =  Quote.create([{content: "Blah blah blah. Emotions and stuff", category_id: Category.find_by(name: nature).id}], {content: "This earth is all we have in common", category_id: Category.find_by(name: nature).id})
 quotes_trump =  Quote.create([{}])
 quotes_sports =  Quote.create([{}])
 quotes_abstract =  Quote.create([{}])
